b9168f8c4a0de7925a46e1b61592cbc67f695fa4
armbdevelop <armanbenyaminyan12@gmail.com>
2024-12-29 20:48:49 +0300

    taggit

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-
+from taggit.managers import TaggableManager
 
 # Create your models here.
 
@@ -27,6 +27,7 @@ class Post(models.Model):
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
     objects = models.Manager()
     published = PublishManager()
+    tags = TaggableManager()
 
     class Meta:
         ordering = ['-publish']
