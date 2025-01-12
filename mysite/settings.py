38bfe4b7352e56ec81f2d7bb3942b7e9511eac8e
armbdevelop <armanbenyaminyan12@gmail.com>
2025-01-12 21:51:35 +0300

    blog is read

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,8 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+
+DEBUG = bool(os.getenv('DEBUG', True))
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 SITE_ID = 1
 
